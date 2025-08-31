a91ef1c00e19e4befa4a079e74e5caf37c5378d4
Touki <Touki@pc.com>
2025-08-31 10:37:33 +0800

    feat: 增加开屏展示
    现在功能比较少，开屏时间比较短，因此加了2s显示时间（main_windows.MainWindow.splashScreenShowTime），后续如果功能多，加载时间长一点了就可以取消这个方法

--- app/views/main_window.py
+++ app/views/main_window.py
@@ -1,9 +1,9 @@
 import sys
 
-from PyQt5.QtCore import Qt
+from PyQt5.QtCore import Qt, QSize, QEventLoop, QTimer
 from PyQt5.QtGui import QIcon
 from PyQt5.QtWidgets import QApplication
-from qfluentwidgets import FluentWindow, FluentIcon, NavigationItemPosition
+from qfluentwidgets import FluentWindow, FluentIcon, NavigationItemPosition, SplashScreen
 
 import app.resource.resources_rc
 from app.views.home_interface import HomeInterface
@@ -14,11 +14,19 @@ class MainWindow(FluentWindow):
     def __init__(self):
         super().__init__()
 
+        self.initWindow()
+
+        self.splashScreen = SplashScreen(self.windowIcon(), self)
+        self.splashScreen.setIconSize(QSize(106, 106))
+        self.show()
+
+        self.splashScreenShowTime()
+
         self.homeInterface = HomeInterface(self)
         self.videoFormatInterface = VideoFormatInterface()
 
         self.initNavigation()
-        self.initWindow()
+        self._onFinished()
 
     def initNavigation(self):
         self.addSubInterface(self.homeInterface, FluentIcon.HOME, '主页', isTransparent=True)
@@ -38,6 +46,7 @@ class MainWindow(FluentWindow):
 
     def initWindow(self):
         self.resize(1000, 700)
+        self.setMinimumWidth(760)
         self.setWindowIcon(QIcon(':/icons/China_Railways.svg'))
         self.setWindowTitle('QtTools')
 
@@ -45,6 +54,13 @@ class MainWindow(FluentWindow):
         desktopW, desktopH = desktop.width(), desktop.height()
         self.move(desktopW // 2 - self.width() // 2, desktopH // 2 - self.height() // 2)
 
+    def _onFinished(self):
+        self.splashScreen.finish()
+
+    def splashScreenShowTime(self):
+        loop = QEventLoop(self)
+        QTimer.singleShot(2000, loop.quit)
+        loop.exec()
 
 if __name__ == '__main__':
     QApplication.setHighDpiScaleFactorRoundingPolicy(Qt.HighDpiScaleFactorRoundingPolicy.PassThrough)
